9b682962cb37e7c7541e7bec434407b4f6475b6d
kousik2000 <kousik.ramachandruni@gmail.com>
2023-07-19 13:11:27 +0530

    update

--- index.js
+++ index.js
@@ -7,88 +7,80 @@ const Blog = require('./blog');
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:3000'
-}))
+    origin: 'https://portfolio-frontend-rose-seven.vercel.app'
+}));
 
 app.use(express.json());
 
 mongoose.set('strictQuery', false);
 
-
 mongoose.connect('mongodb+srv://user1234:hellouser@cluster0.jjghdhu.mongodb.net/?retryWrites=true&w=majority', {
-    useUnifiedTopology:true,
-    useNewUrlParser:true
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 }).then(
-    ()=> console.log('DB connected')
-).catch(err=> console.log(err));
+    () => console.log('DB connected')
+).catch(err => console.log(err));
 
- 
-app.post('/portfolio',async (req, res)=>{
-    const {title,url,imageUrl,github} = req.body;
-    try{
-        const newData = new Portfolio({title,url,imageUrl,github});
+// Portfolio Routes
+
+app.post('/portfolio', async (req, res) => {
+    const { title, url, imageUrl, github } = req.body;
+    try {
+        const newData = new Portfolio({ title, url, imageUrl, github });
         await newData.save();
-        return res.json(await Portfolio.find())
-    }
-    catch(err){
+        return res.json(await Portfolio.find());
+    } catch (err) {
         console.log(err.message);
     }
-})
+});
 
-app.get('/getportfoliodata',async (req,res)=>{
-    try{
+app.get('/getportfoliodata', async (req, res) => {
+    try {
         const allData = await Portfolio.find();
         return res.json(allData);
-    }
-    catch(err){
+    } catch (err) {
         console.log(err.message);
     }
-})
+});
 
-app.delete('/deleteportfolio/:id',async(req,res)=>{
-    try{
+app.delete('/deleteportfolio/:id', async (req, res) => {
+    try {
         await Portfolio.findByIdAndDelete(req.params.id);
-        return res.json(await Portfolio.find())
-    }
-    catch(err){
+        return res.json(await Portfolio.find());
+    } catch (err) {
         console.log(err.message);
     }
-})
+});
 
-//blog section
+// Blog Routes
 
-app.post('/blog',async (req, res)=>{
-    const {title,description,url,imageUrl} = req.body;
-    try{
-        const newData = new Blog({title,description,url,imageUrl});
+app.post('/blog', async (req, res) => {
+    const { title, description, url, imageUrl } = req.body;
+    try {
+        const newData = new Blog({ title, description, url, imageUrl });
         await newData.save();
-        return res.json(await Blog.find())
-    }
-    catch(err){
+        return res.json(await Blog.find());
+    } catch (err) {
         console.log(err.message);
     }
-})
+});
 
-app.get('/getblogdata',async (req,res)=>{
-    try{
+app.get('/getblogdata', async (req, res) => {
+    try {
         const allData = await Blog.find();
         return res.json(allData);
-    }
-    catch(err){
+    } catch (err) {
         console.log(err.message);
     }
-})
+});
 
-app.delete('/deleteblog/:id',async(req,res)=>{
-    try{
+app.delete('/deleteblog/:id', async (req, res) => {
+    try {
         await Blog.findByIdAndDelete(req.params.id);
-        return res.json(await Blog.find())
-    }
-    catch(err){
+        return res.json(await Blog.find());
+    } catch (err) {
         console.log(err.message);
     }
-})
+});
 
-  
-
-app.listen(9000,() => console.log('server running'));
+app.listen(9000, () => console.log('Server running on port 9000'));
